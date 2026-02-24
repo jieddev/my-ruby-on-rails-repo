3e62bba8478b021515e8aa8a0bbb5c1d73f08a8e
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-02-24 15:55:51 +0800

    Add create and product_params method in the products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,4 +11,20 @@ class ProductsController < ApplicationController
     @product = Product.new
   end
 
+  def create 
+    @product = Product.new(product_params)
+
+    if @product.save 
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private 
+
+    def product_params 
+      params.expect(product: [:name])
+    end
+
 end
