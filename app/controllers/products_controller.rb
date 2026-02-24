8d3af3b30bc3f402593d0a7083d5c6812f3315e2
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-02-24 14:09:53 +0800

    Update products_controller to add multiple methods

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,52 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create 
+    @product = Product.new(product_params)
+
+    if @product.save 
+      redirect_to @product, notice: "Product created successfully."
+    else
+      render: new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to @product, notice: "Product updated successfully."
+    else 
+      render :edit 
+    end
+
+  end
+
+  def destroy 
+    @product.destroy 
+    redirect_to producst_path, notice: "Product deleted successfully."
+  end
+
+  private 
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params 
+    params.require(:product).permit(:name, :description, :price, :stock)
+  end
+
 end
