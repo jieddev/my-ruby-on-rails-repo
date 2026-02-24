d2e62336951055c2a0e466218184671e3de8b67d
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-02-24 15:38:32 +0800

    Initial Commit

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,14 @@
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all 
+  end
+
+  def show 
+    @product = Product.find(params[:id])
+  end
+
+  def new 
+    @product = Product.new
+  end
+
+end
