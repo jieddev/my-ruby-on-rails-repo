e8fa4157ec44a864502cbe1c3a824c0e520b1452
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-02-25 14:06:10 +0800

    Refactor Product model to execute display_product variable after_create

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,14 @@
 class Product < ApplicationRecord
+  after_create :display_product
+
   validates :name, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :stock, numericality: { greater_than_or_equal_to: 0 }
+
+  private 
+    def display_product
+      puts "Product is dispayed"
+    end
+
+
 end
