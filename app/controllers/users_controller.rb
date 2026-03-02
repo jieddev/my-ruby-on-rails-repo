90fdd1f1d776cfa13bdd1036ef3538b708994d2b
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-03-02 14:25:51 +0800

    Add edit.html.erb and update routes.rb and users controller to support #edit and #update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,22 @@
 class UsersController < ApplicationController
+
   def new 
+    @user = User.new()
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
 
   def create
     # @user = User.new(user_params)
@@ -13,7 +29,15 @@ class UsersController < ApplicationController
     end
   end
 
+  def show 
+  end
+
   private 
+
+  def set_user 
+    @user = User.find(params[:id])
+  end
+
 
   def user_params 
     params.require(:user).permit(:username, :email, :password)
