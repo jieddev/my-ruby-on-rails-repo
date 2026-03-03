85cf5abea779497769c4bca167f2eaf1ebea438c
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-03-03 14:36:44 +0800

    Display the created event in the events page after submitting form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,33 @@
 class EventsController < ApplicationController
   def index
+    @event = Event.new
   end
+
+  # def new 
+  #   @event = Event.new
+  # end
+
+  def show 
+    @event = Event.find(params[:id])
+  end
+
+
+  def create 
+    @event = current_user.events.build(event_params) 
+
+    if @event.save
+      redirect_to @event
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+  end
+
+  private 
+
+    def event_params
+      params.require(:event).permit(:title, :description)
+    end
+
+
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,5 @@
 class Event < ApplicationRecord
-  belongs_to :creator, class_name: "User", foreign_key: "creator_id"
-  
-
-  def index
-    @events = Event.all 
-  end
-
+  belongs_to :creator, class_name: "User", foreign_key: "user_id"
 
 
 end
