cada1d6e26f7524c41b87b2cfd83a842db32e353
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-03-04 10:33:19 +0800

    Add validations for :title and :description to make sure that it is present

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User", foreign_key: "user_id"
 
+  validates :title, presence: true 
+  validates :description, presence: true 
 
 end
