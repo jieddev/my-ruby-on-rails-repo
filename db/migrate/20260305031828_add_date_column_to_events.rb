8e82dda115cc18d32e4d9c7134395ac9e016792f
jieddev <jiedelflorentino.ruizo@gmail.com>
2026-03-05 11:29:52 +0800

    Refactor user/show.html.erb to display past and upcoming events and update the Event model to have a date column with datetime data type

---
+++ db/migrate/20260305031828_add_date_column_to_events.rb
@@ -0,0 +1,5 @@
+class AddDateColumnToEvents < ActiveRecord::Migration[8.1]
+  def change
+    add_column :events, :date, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_04_014036) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_05_031828) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -25,6 +25,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_04_014036) do
 
   create_table "events", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.datetime "date"
     t.text "description"
     t.string "title"
     t.datetime "updated_at", null: false
